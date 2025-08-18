410b1a23941221966c1e9bb1ed6140ebf653dcbd
BabaVo55 <ethemcode@outlook.com>
2025-08-18 15:19:42 +0100

    flash san

--- classes.js
+++ classes.js
@@ -2,7 +2,7 @@ class Sprite {
     constructor({position, image, frames = {max: 1}}){
         this.position = position
         this.image = image;
-        this.frames = frames;
+        this.frames = {...frames, val: 0};
 
         this.image.onload = () => {
             this.width = this.image.width / this.frames.max;
@@ -15,7 +15,7 @@ class Sprite {
         c.drawImage(
             this.image,
             // Cropping
-            0,
+            this.frames.val,
             0,
             this.image.width / this.frames.max,
             this.image.height, 
@@ -25,6 +25,11 @@ class Sprite {
             this.image.width / this.frames.max,
             this.image.height, 
         )
+        if (this.frames.val < this.frames.max){
+            this.frames.val++
+        } else {
+            this.frames.val = 0
+        }
     }   
 }
 
