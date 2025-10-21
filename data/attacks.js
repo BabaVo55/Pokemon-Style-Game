c74afc39f1f9c73b593157134cb03bd41a73faee
BabaVo55 <ethemcode@outlook.com>
2025-10-21 11:31:32 +0100

    attack loop map type

---
+++ data/attacks.js
@@ -0,0 +1,13 @@
+const attacks = {
+    Tackle : {
+                name: 'Tackle',
+                damage: 10,
+                type: 'Normal'
+            },
+    YogaFlame : {
+                name: 'YogaFlame',
+                damage: 25,
+                type: 'Fire'
+            }
+    
+}
