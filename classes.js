8ea603afd8770bd2b047e8b697ecb1347f4566c5
BabaVo55 <ethemcode@outlook.com>
2025-10-21 16:01:29 +0100

    fireball attack rotated  it think

--- classes.js
+++ classes.js
@@ -1,23 +1,27 @@
 class Sprite {
     constructor({position, image, frames = {max: 1, hold: 10}, sprites = [], animate = false, isEnemy = false}){
-        this.position = position
-        this.image = image;
-        this.frames = {...frames, val: 0, elapsed: 0};
+        this.position = position,
+        this.image = image,
+        this.frames = {...frames, val: 0, elapsed: 0},
 
         this.image.onload = () => {
-            this.width = this.image.width / this.frames.max;
-            this.height = this.image.height;
+            this.width = this.image.width / this.frames.max,
+            this.height = this.image.height
         }
-        this.sprites = sprites;
-        this.animate = animate;
+        this.sprites = sprites,
+        this.animate = animate,
         this.opacity = 1; // save / initialize the state desired
-        this.health = 100
-        this.isEnemy = isEnemy;
+        this.health = 100,
+        this.isEnemy = isEnemy,
+        this.rotation = 0
         
     }
   
     draw(){
         c.save()
+        c.translate(this.position.x + this.width / 2, this.position.y + this.height / 2)
+        c.rotate(this.rotation)
+        c.translate(-this.position.x - this.width / 2, -this.position.y - this.height / 2)
         c.globalAlpha = this.opacity;
         c.drawImage(
             this.image,
@@ -127,7 +131,8 @@ class Sprite {
                         max: 4,
                         hold: 10
                     },
-                    animate: true
+                    animate: true,
+                    rotation: 1
                         
                 })
 
